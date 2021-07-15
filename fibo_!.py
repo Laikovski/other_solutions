3683646dd8442e107bd87383ba49a98a91377430
laikov <zheka-se@ya.ru>
2021-07-15 12:09:41 +0300

    15-07-02

--- fibo_!.py
+++ fibo_!.py
@@ -1,12 +1,22 @@
 """Вычислите первые 100 чисел Фибоначчи. (Напишите код.)"""
 
+# def fibo(num):
+#     f1 = 1
+#     f2 = 1
+#     print(f1, f2, end=' ')
+#     while num > 2:
+#         f1, f2 = f2, f1 + f2
+#         print(f2, end=' ')
+#         num -= 1
+# fibo(100)
+
 def fibo(num):
     f1 = 1
     f2 = 1
     print(f1, f2, end=' ')
-    while num > 2:
+    for i in range(2, num):
         f1, f2 = f2, f1 + f2
-        print(f2, end=' ')
-        num -= 1
-fibo(100)
 
+        print(f2, end=' ')
+
+fibo(60)
