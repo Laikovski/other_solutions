58fc747cca45e4a2ed72ebe6f5ec21a5b119928e
laikov <zheka-se@ya.ru>
2021-07-18 20:44:47 +0300

    18-07-02

--- oop.py
+++ oop.py
@@ -377,9 +377,88 @@ class UserMail:
             print("Ошибочная почта")
     email = property(fget=get_email, fset=set_email)
 
-k = UserMail('belosnezhka', 'prince@wait.you')
-print(k.email)  # prince@wait.you
-k.email = [1, 2, 3] # Ошибочная почта
-k.email = 'prince@still@.wait'  # Ошибочная почта
-k.email = 'prince@still.wait'
-print(k.email)  # prince@still.wait
+# k = UserMail('belosnezhka', 'prince@wait.you')
+# print(k.email)  # prince@wait.you
+# k.email = [1, 2, 3] # Ошибочная почта
+# k.email = 'prince@still@.wait'  # Ошибочная почта
+# k.email = 'prince@still.wait'
+# print(k.email)  # prince@still.wait
+
+"""Создайте класс Money, у которого есть:
+
+    - конструктор __init__, принимающий 2 аргумента: dollars, cents. По входным аргументам вам необходимо создать 
+    атрибут экземпляра total_cents. 
+    - свойство геттер dollars, которое возвращает количество имеющихся долларов;
+    - свойство сеттер dollars, которое принимает целое неотрицательное число - количество долларов и устанавливает 
+    при помощи него новое значение в атрибут экземпляра total_cents, при этом значение центов должно сохранятся. В 
+    случае, если в сеттер передано число, не удовлетворяющее условию, нужно печатать на экран сообщение "Error dollars";
+    - свойство геттер cents, которое возвращает количество имеющихся центов;
+    - свойство сеттер cents, которое принимает целое неотрицательное число меньшее 100 - количество центов и 
+    устанавливает при помощи него новое значение в атрибут экземпляра total_cents, при этом значение долларов должно сохранятся. 
+    В случае, если в сеттер передано число, не удовлетворяющее условию, нужно печатать на экран сообщение "Error cents";
+    метод __str__ (информация по данному методу), который возвращает строку вида "Ваше состояние составляет {dollars} 
+    долларов {cents} центов". Для нахождения долларов и центов в методе __str__ пользуйтесь свойствами
+
+В экземпляр класса кроме атрибута total_cents сохранять ничего не нужно!"""
+
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents
+    @property
+    def dollars(self):
+        return self.total_cents // 100
+    @dollars.setter
+    def dollars(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.total_cents = self.total_cents - self.total_cents //100*100 + value * 100
+        else:
+            print('Error dollars')
+    @property
+    def cents(self):
+        return self.total_cents % 100
+    @cents.setter
+    def cents(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.total_cents = self.total_cents - self.total_cents % 100 + int(value)
+        else:
+            print('Error cents')
+    def __str__(self):
+        return f'Ваше состояние составляет {self.total_cents // 100} долларов {self.total_cents % 100} центов'
+
+
+# Bill = Money(101, 99)
+# print(Bill)  # Ваше состояние составляет 101 долларов 99 центов
+# print(Bill.dollars, Bill.cents)  # 101 99
+# Bill.dollars = 666
+# print(Bill)  # Ваше состояние составляет 666 долларов 99 центов
+# Bill.cents = 12
+# print(Bill)  # Ваше состояние составляет 666 долларов 12 центов
+
+
+class Square:
+
+    def __init__(self, side):
+        self.__side = side
+        self.__p = None
+    @property
+    def side(self):
+        return self.__side
+
+    @side.setter
+    def side(self, value):
+        self.__side = value
+        self.__p = None
+
+    @property
+    def area(self):
+        if self.__p is None:
+            print('calc')
+            self.__p = self.side ** 2
+        return self.__p
+
+
+s = Square(4)
+print(s.area)
+s.side = 2
+print(s.area)
+print(s.area)
